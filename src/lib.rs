b084522e13da2335dd0b4a4343b9463817063489
rust-pgrn <rust_pgrn@proton.me>
2022-07-23 17:19:41 +0200

    basic outline for project

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub fn contents() {}
+pub fn clean() {}
+pub fn split() {}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_contents() {}
+    #[test]
+    fn test_clean() {}
+    #[test]
+    fn test_split() {}
+}
