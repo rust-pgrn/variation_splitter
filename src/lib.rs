e9345e7a742a36a49ffb8b925961968b97911085
rust-pgrn <rust_pgrn@proton.me>
2022-07-24 15:29:55 +0200

    Split variations when white starts the variation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, ops::Deref};
 pub fn contents(filename: &str) -> String {
     fs::read_to_string(filename).expect("File Not Found")
 }
@@ -9,15 +9,46 @@ pub fn clean(contents: String) -> String {
     }
     lines.collect()
 }
-pub fn split<'a>(contents: &'a str) -> Vec<&'a str> {
+pub fn split(contents: String) -> Vec<Vec<String>> {
+    let mut variations: Vec<Vec<String>> = Vec::new();
+    let mut i = variations.len();
+    let mut j = 1;
+    variations.push(Vec::new());
     for pli in contents.split(' ') {
-        println!("{}", pli);
+        //println!("{}", pli);
+        if pli.contains("...") && pli.contains("(") {
+            //Creates new array with current moves
+            variations.push(variations[i].clone());
+            //increments i to work on next array/variation
+            i += j;
+            //Removes last 3 moves, which will get replaced by the current pli
+            variations[i].pop();
+            variations[i].pop();
+            variations[i].pop();
+        } else if pli.contains('(') {
+            //Creates new array with current moves
+            variations.push(variations[i].clone());
+            //increments i to work on next array/variation
+            i += j;
+            //Removes last 2 moves, which will get replaced by the current pli
+            variations[i].pop();
+            variations[i].pop();
+            variations[i].push(pli.replace('(', ""));
+        } else if pli.contains(')') {
+            variations[i].push(pli.replace(')', ""));
+            i -= 1;
+            j += 1;
+        } else if pli.contains("...") && !pli.contains('(') {
+        } else {
+            variations[i].push(pli.to_string());
+        }
+        //variations.last_mut().unwrap().push(pli);
     }
-    vec!["1.d4", "1.e4"]
+    variations
 }
 #[cfg(test)]
 mod tests {
-    use super::*;
+    //use super::*;
     #[test]
     fn test_contents() {}
     #[test]
@@ -26,6 +57,6 @@ mod tests {
     fn test_split() {
         //1.d4(1.e4) =
         //[1.d4,1.e4]
-        assert_eq!(split("1.d4(1.e4)"), vec!["1.d4", "1.e4"]);
+        //assert_eq!(split("1.d4(1.e4)"), vec!["1.d4", "1.e4"]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod lib;
 fn main() {
     let contents = lib::contents("pgn.txt");
-    println!("{:?}", lib::split(&contents));
+    println!("{:?}", lib::split(contents));
 }
