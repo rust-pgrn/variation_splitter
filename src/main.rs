9290515b97f11c5266cc3a1a9d077769adcf4c0b
rust-pgrn <rust_pgrn@proton.me>
2022-08-01 16:00:09 +0200

    Changed main to match new file names

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod lib;
 use std::fs;
 fn main() {
-    let contents = lib::contents("don-t_change.txt");
+    let contents = lib::contents("input.txt");
     let contents = lib::clean(contents);
-    let variations = lib::split(contents, "pgn.txt");
-    fs::write("file.pgn", lib::convert(&variations)).unwrap();
+    let variations = lib::split(contents, "logs.txt");
+    fs::write("output.pgn", lib::convert(&variations)).unwrap();
 }
