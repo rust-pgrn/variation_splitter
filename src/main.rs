4f4a84bf80c065fbd10bef4f743b93af05371602
rust-pgrn <rust_pgrn@proton.me>
2022-08-02 13:53:35 +0200

    Added file dir to contain non-code files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod lib;
 use std::fs;
 fn main() {
-    let contents = lib::contents("input.txt");
+    let contents = lib::contents("files/input.txt");
     let contents = lib::clean(contents);
-    let variations = lib::split(contents, "logs.txt");
-    fs::write("output.pgn", lib::convert(&variations)).unwrap();
+    let variations = lib::split(contents, "files/logs.txt");
+    fs::write("files/output.pgn", lib::convert(&variations)).unwrap();
 }
